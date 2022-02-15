aae843eca5475af9d202474430c360adbd54a6d5
shikhar-srivastava <shikharsrivastav@gmail.com>
2022-02-15 11:39:50 +0400

    update

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,9 +1,23 @@
-python3 ../main.py --gpu $1 \
+#!/bin/bash
+#SBATCH --partition=long
+#SBATCH --job-name=meta
+#SBATCH --time=12:00:00
+#SBATCH -N1
+#SBATCH -n1
+#SBATCH --cpus-per-task=32
+#SBATCH --mem=20G
+#SBATCH --gres=gpu:1
+#SBATCH --output=/l/users/shikhar.srivastava/workspace/TANS/scripts/%j.out
+#SBATCH -q gpu-single
+
+ulimit -u 10000
+
+/home/shikhar.srivastava/miniconda3/envs/tans_a/bin/python ../main.py --gpu 0 \
                    --mode train \
                    --batch-size 140 \
                    --n-epochs 10000 \
-                   --base-path /v8/wyjeong/research/tans_test\
-                   --data-path /v8/wyjeong/research/tans_test/data\
-                   --model-zoo /v8/wyjeong/research/tans_test/data/model_zoo.pt\
-                   --model-zoo-raw /v14/geon/samsung_model_zoo/ofa_models\
-                   --seed 777 
+                   --base-path /l/users/shikhar.srivastava/workspace/TANS/outcomes \
+                   --data-path /l/users/shikhar.srivastava/data/ofa/data_path \
+                   --model-zoo /l/users/shikhar.srivastava/data/ofa/model_zoo/p_mod_zoo.pt \
+                   --model-zoo-raw /l/users/shikhar.srivastava/data/ofa/model_zoo_raw \
+                   --seed 777
